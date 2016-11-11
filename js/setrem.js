5438d23fe2da8f6f68d56cf94f9eecb11c850f0c
unknown <>
2016-11-11 18:01:43 +0800

    yh

--- js/setrem.js
+++ js/setrem.js
@@ -1,3 +1,33 @@
-  var deviceWidth = document.documentElement.clientWidth;
-if(deviceWidth >640) deviceWidth = 640;
+var deviceWidth = document.documentElement.clientWidth;
+//alert(deviceWidth)
+if(deviceWidth >640){
+	deviceWidth = 640;
+}
+
+
 document.documentElement.style.fontSize = deviceWidth / 6.4*2 + 'px';
+//(function() {
+//  var b = navigator.userAgent;
+//  ipad = b.match(/(iPad).*OS\s([\d_]+)/) ? true : false;
+//  iphone = !ipad && b.match(/(iPhone\sOS)\s([\d_]+)/) ? true : false;
+//  uc = navigator.userAgent.indexOf("UCBrowser") > -1 ? true : false;
+//  ios = ipad || iphone;
+//  var d = 1,a = 1,c;
+//  if (ios) {
+//      if (window.devicePixelRatio >= 2) {
+//          d *= 0.5;
+//          a *= 2
+//      }
+//  } else {
+//      if (uc) {
+//          if (window.devicePixelRatio >= 2) {
+//              d *= 0.5;
+//              a *= 2
+//          }
+//      }
+//  }
+//  c = '<meta name="viewport" content="initial-scale=' + d + ", maximum-scale=" + d + ", minimum-scale=" + d + ', width=device-width, user-scalable=no" />';
+//  document.write(c);
+//  alert(a)
+//  document.documentElement.style.fontSize = 100 * a + "px"
+//})();
--- public/public.js
+++ public/public.js
@@ -13,51 +13,64 @@ function dialoginfo(msg, fn) {
 	mui.alert(msg, fn)
 }
 
-function isMobile(phone){
-  // 根据号码段的不同和前缀不同可适当修改
-  var reValue = /^(0|86|17951)?(13[0-9]|15[012356789]|18[0-9]|14[57]|17[0-9])[0-9]{8}$/;
-  if(!reValue.test(phone)){
-    return false;
-  }else{
-    return true;
-  }
-} 
-
-function isEmail(email){
-  // @符号前后直接相邻不能为 .
-  var reEmail = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
-  if(!reEmail.test(email)){
-    return false;
-  }else{
-    return true;
-  }
-} 
-
-function isQQ(qq){
-  var reQQ = /^[1-9]\d{4,9}$/; 
-  if(!reQQ.test(qq)){
-    return false;
-  }else{
-    return true;
-  }
-} 
-function isbankcard(b){
-	var reg = /^\d{19}$/g; // 以19位数字开头，以19位数字结尾 
-		if( !reg.test(b) ){ 
-		return false
-		}else{
-			return true
-		}
+function isMobile(phone) {
+	// 根据号码段的不同和前缀不同可适当修改
+	var reValue = /^(0|86|17951)?(13[0-9]|15[012356789]|18[0-9]|14[57]|17[0-9])[0-9]{8}$/;
+	if(!reValue.test(phone)) {
+		return false;
+	} else {
+		return true;
+	}
 }
-function sj(obj){
+
+function isEmail(email) {
+	// @符号前后直接相邻不能为 .
+	var reEmail = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+	if(!reEmail.test(email)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function isQQ(qq) {
+	var reQQ = /^[1-9]\d{4,9}$/;
+	if(!reQQ.test(qq)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function isbankcard(b) {
+	var reg = /^\d{19}$/g; // 以19位数字开头，以19位数字结尾 
+	if(!reg.test(b)) {
+		return false
+	} else {
+		return true
+	}
+}
+
+function sj(obj) {
 	console.log(JSON.stringify(obj))
 }
 
-function clearString(s){ 
-    var pattern = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&;|{}【】‘；：”“'。，、？]") 
-    var rs = ""; 
-    for (var i = 0; i < s.length; i++) { 
-        rs = rs+s.substr(i, 1).replace(pattern, ''); 
-    } 
-    return rs;  
-} 
+function clearString(s) {
+	var pattern = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&;|{}【】‘；：”“'。，、？]")
+	var rs = "";
+	for(var i = 0; i < s.length; i++) {
+		rs = rs + s.substr(i, 1).replace(pattern, '');
+	}
+	return rs;
+}
+
+function CheckImgExists(imgurl) {
+	var ImgObj = new Image(); //判断图片是否存在
+	ImgObj.src = imgurl;
+	//没有图片，则返回-1
+	if(ImgObj.fileSize > 0 || (ImgObj.width > 0 && ImgObj.height > 0)) {
+		return true;
+	} else {
+		return false;
+	}
+}
